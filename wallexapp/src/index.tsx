11a83c8dd5dd40079b05c434349caace8fa08138
PoznyakM <vash.lavashik@gmail.com>
2023-08-29 02:45:42 +0300

    \#1: routing & some components added

--- wallexapp/src/index.tsx
+++ wallexapp/src/index.tsx
@@ -1,16 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/app.scss';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider, Navigate, Outlet } from 'react-router-dom';
+import './styles/fontFaces.css';
+import MainContainer from './views/MainContainer';
+import WalletView from './views/WalletView';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainContainer footer />,
+    children: [
+      {
+        index: true,
+        element: <WalletView />,
+      },
+      {
+        path: '/currency',
+        element: <p>currency</p>,
+      },
+      {
+        path: '/help',
+        element: <p>help</p>,
+      },
+    ],
+  },
+  {
+    path: '/profile',
+    element: (
+      <div>
+        <p>kekekeke</p>
+      </div>
+    ),
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ wallexapp/src/views/WalletView/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Heading from '../../ui/Heading';
+
+type Props = {};
+
+function WalletView({}: Props) {
+  return (
+    <div>
+      <Heading title="Мои финансы" />
+    </div>
+  );
+}
+
+export default WalletView;
