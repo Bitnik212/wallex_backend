480eb3acfbc334928d86c483cbce6db8718fef32
Nikolay Nikonorov <bit@bitt.moe>
2023-08-29 16:49:53 +0300

    get currencies api

--- backend/restapi/controllers/__init__.py
+++ backend/restapi/controllers/__init__.py
@@ -2,15 +2,16 @@ __author__ = "Николай Витальевич Никоноров (Bitnik212)
 __date__ = "29.08.2023 05:28"
 
 from django.urls import path
-from django.views.generic import TemplateView
 from rest_framework.urlpatterns import format_suffix_patterns
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from .AuthUserController import AuthUserController
+from .CurrencyController import CurrencyController
 from .UserController import UserController
 
 urlpatterns = [
     path("user/create", UserController.as_view()),
     path("user/", AuthUserController.as_view()),
+    path("bank/currency/", CurrencyController.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
